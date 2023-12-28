16df2d68f4326833629c712343fcb67bbe029b0a
jsy3264 <152372593+jsy3264@users.noreply.github.com>
2023-12-28 15:08:40 +0900

    build

---
+++ js/about.4e220bf1.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkweb3"]=self["webpackChunkweb3"]||[]).push([[443],{6917:function(n,e,t){t.r(e),t.d(e,{default:function(){return l}});var r=t(3396);const s={class:"section-container"},u=(0,r._)("h2",null,"This is an about page",-1),a=[u];function c(n,e,t,u,c,i){return(0,r.wg)(),(0,r.iD)("div",s,a)}var i={},o=t(89);const f=(0,o.Z)(i,[["render",c]]);var l=f}}]);
+//# sourceMappingURL=about.4e220bf1.js.map
